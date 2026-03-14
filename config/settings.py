9554fdbb4ed3f600fddf6078f0d5b1d71a526cb2
baxtiyorovbekzod <bekzodbaxtiyorov142@gmail.com>
2026-03-14 08:47:09 +0500

    ok

--- config/settings.py
+++ config/settings.py
@@ -140,7 +140,7 @@ if DJANGO_ENV == 'development':
         BASE_DIR / 'static'
     ]
 elif DJANGO_ENV == 'production':
-    STATIC_ROOT = BASE_DIR / 'static'
+    STATIC_ROOT =Path("/home/django/django-blog-website/static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
